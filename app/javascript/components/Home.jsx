f02c76e4d922a2696f7ab69e0735be9048942d7b
realtradam <github@tradam.dev>
2024-03-09 18:02:08 -0500

    Configure react as the frontend

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Games!</h1>
+        <p className="lead">
+			All the games I have worked on that run on the web!
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/games"
+          className="btn btn-lg custom-button"
+          role="button"
+        >
+          View Games
+        </Link>
+      </div>
+    </div>
+  </div>
+);
