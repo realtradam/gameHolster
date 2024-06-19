2870bd2c4e4053d5187df6d1ae1cae2c6851d01d
realtradam <github@tradam.dev>
2024-06-19 18:39:51 -0400

    rename to game holster

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'tomlib'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GamesHost
+module GameHolster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
