9eb3ab91fb653ca6d4921238e2a497427405f178
realtradam <github@tradam.dev>
2024-06-28 17:25:36 -0400

    add login

---
+++ react-frontend/src/pages/CloseWindow.tsx
@@ -0,0 +1,19 @@
+
+export default function CloseWindow () {
+
+	// trigger localStorage listener in other tabs.
+	// this forces react rerender where required.
+	localStorage.setItem("logged in trigger", String(Math.random()));
+	// close window once done
+	window.close();
+
+	// window can only be closed if it was opened by javascript.
+	// if the window was opened by a user then just redirect to
+	// the home page instead.
+	window.location.replace("/");
+
+	return(
+		<>
+		</>
+	);
+}
